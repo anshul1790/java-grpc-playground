9cf4db43a97ccb2ba74af367177c93a9c1fb4824
Anshul Choudhary <achoudhary@chegg.com>
2024-05-27 09:44:17 +0530

    repo set up

---
+++ src/main/java/com/learn/javagrpcplayground/JavaGrpcPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.learn.javagrpcplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGrpcPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGrpcPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/learn/javagrpcplayground/sec01/SimpleGrpcDemoServerService.java
@@ -0,0 +1,5 @@
+package com.learn.javagrpcplayground.sec01;
+
+public class SimpleGrpcDemoServerService {
+
+}
