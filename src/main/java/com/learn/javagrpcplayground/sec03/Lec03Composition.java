dda9bef59af6350bba10585449eeed919bad0ff6
Anshul Choudhary <achoudhary@chegg.com>
2024-05-27 22:09:07 +0530

    composition

---
+++ src/main/java/com/learn/javagrpcplayground/sec03/Lec03Composition.java
@@ -0,0 +1,32 @@
+package com.learn.javagrpcplayground.sec03;
+
+import com.grpc.sec03.composition.service.Address;
+import com.grpc.sec03.composition.service.School;
+import com.grpc.sec03.composition.service.Student;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Lec03Composition {
+
+    public static void main(String[] args) {
+        // create student
+        var address = Address.newBuilder()
+                .setStreet("123 main st")
+                .setCity("Atlanta")
+                .setState("GA")
+                .build();
+        var student = Student.newBuilder()
+                .setAddress(address)
+                .setName("sam")
+                .build();
+
+        // create school
+        var school = School.newBuilder()
+                .setId(1)
+                .setName("high school")
+                .setAddress(address.toBuilder().setStreet("234 main street"))
+                .build();
+        log.info("student: {}", student);
+        log.info("school: {}", school);
+    }
+}
