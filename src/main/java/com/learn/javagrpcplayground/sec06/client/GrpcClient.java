cb99581ac691d60d35c90717bb74007073374fef
Anshul Choudhary <achoudhary@chegg.com>
2024-06-01 22:32:16 +0530

    added client channel and stubs

--- src/main/java/com/learn/javagrpcplayground/sec06/client/GrpcClient.java
+++ src/main/java/com/learn/javagrpcplayground/sec06/client/GrpcClient.java
@@ -1,22 +1,50 @@
 package com.learn.javagrpcplayground.sec06.client;
 
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
+import sec06.AccountBalance;
 import sec06.BalanceCheckRequest;
 import sec06.BankServiceGrpc;
 
 @Slf4j
 public class GrpcClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         var channel = ManagedChannelBuilder
                 .forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
-        var stub = BankServiceGrpc.newBlockingStub(channel);
+        /*var stub = BankServiceGrpc.newBlockingStub(channel);
         var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder()
                 .setAccountNumber(2)
                 .build());
-        log.info("{}", balance);
+        log.info("{}", balance);*/
+
+
+        var stub = BankServiceGrpc.newStub(channel);
+        stub.getAccountBalance(BalanceCheckRequest.newBuilder()
+                .setAccountNumber(2)
+                .build(), new StreamObserver<>() {
+            @Override
+            public void onNext(AccountBalance value) {
+                log.info("Balance is {}", value.getBalance());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("completed");
+            }
+        });
+
+        log.info("Done");
+
+        Thread.sleep(1000);
+
     }
 }
