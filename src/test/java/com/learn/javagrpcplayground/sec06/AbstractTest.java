7516337b36e970815bed00378281d1487eb66502
Anshul Choudhary <achoudhary@chegg.com>
2024-06-01 22:46:25 +0530

    added unit test for unary clients

---
+++ src/test/java/com/learn/javagrpcplayground/sec06/AbstractTest.java
@@ -0,0 +1,24 @@
+package com.learn.javagrpcplayground.sec06;
+
+import com.learn.javagrpcplayground.common.AbstractChannelTest;
+import com.learn.javagrpcplayground.common.GrpcServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import sec06.BankServiceGrpc;
+
+public abstract class AbstractTest extends AbstractChannelTest {
+
+    private final GrpcServer grpcServer = GrpcServer.create(new BankServiceImpl());
+    protected BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @BeforeAll
+    public void setup() {
+        this.grpcServer.start();
+        this.blockingStub = BankServiceGrpc.newBlockingStub(channel);
+    }
+
+    @AfterAll
+    public void stop() {
+        this.grpcServer.stop();
+    }
+}
---
+++ src/test/java/com/learn/javagrpcplayground/sec06/UnaryBlockingClientTest.java
@@ -0,0 +1,28 @@
+package com.learn.javagrpcplayground.sec06;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sec06.BalanceCheckRequest;
+
+public class UnaryBlockingClientTest extends AbstractTest {
+
+    private static final Logger log = LoggerFactory.getLogger(UnaryBlockingClientTest.class);
+
+    @Test
+    public void getBalanceTest() {
+        var request = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(1)
+                .build();
+
+        var balance = this.blockingStub.getAccountBalance(request);
+        log.info("Unary balance: {}", balance);
+        Assertions.assertEquals(100, balance.getBalance());
+
+    }
+
+
+
+}
